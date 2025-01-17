fc3c36fd6ffbd9b0196157046d9b966c0f9bf6f8
Beastmaster69420 <rammhrzn0000@gmail.com>
2025-01-17 19:47:08 +0545

    file structure update

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
 const express = require('express');
-const fs = require('fs');
+const morgan = require('morgan');
 const app = express();
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+const tourRouter=require('./routers/tourRouter')
+const userRouter=require('./routers/userRouter')
 app.use(express.json());
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  console.log('Middleware executed');
+  next();
 });
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id * 1;
-  if(id>tours.length){
-    return res.status(404).json({
-        status:'fail',
-        message: 'Invalid ID'
-    })
-  }
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
-app.post('/api/v1/tours', (req, res) => {
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.post('/api/v1/tours',createTour);
+// app.patch('/api/v1/tours/:id',updateTour)
+// app.delete('/api/v1/tours/:id',deleteTour)
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Response from server', app: 'Natours' });
 // });
 // app.post('/',(req,res)=>{
 //     res.send('Posting is available.')
 // })
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
-});
